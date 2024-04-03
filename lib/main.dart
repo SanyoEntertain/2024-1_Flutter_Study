dce0b300dd4bad9cedeb2adf8adfe260ed5d498b
SanyoEntertain <159179131+SanyoEntertain@users.noreply.github.com>
2024-04-03 18:36:40 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,239 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home : Scaffold(
+          appBar: AppBar(
+              title: Row(
+                children: [
+                  Text(
+                    "서초동",
+                    style: TextStyle(color: Colors.black,),
+                  ),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.arrow_drop_down), color: Colors.black,)
+                ],
+              ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.account_circle_outlined), color: Colors.black,),
+          IconButton(onPressed: (){}, icon: Icon(Icons.search), color: Colors.black,),
+          IconButton(onPressed: (){}, icon: Icon(Icons.notifications_none_outlined), color: Colors.black,),
+        ],
+            backgroundColor: Colors.white,
+          ),
+        body:
+        Container(
+          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+          child:
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                          Column(
+                              children: [
+                                Icon(Icons.account_circle_outlined, size: 50,),
+                                Text('모임 둘러보기'),
+                              ]
+                          ),
+                      Column(
+                          children: [
+                            Image.asset('assets/badminton.webp', width: 50, height: 50,),
+                            Text('배드민턴 구함'),
+                          ]
+                      ),
+                      Column(
+                          children: [
+                            Image.asset('assets/guitar.png', width: 50, height: 50),
+                            Text('기타 구함'),
+                          ]
+                      ),
+                      Column(
+                          children: [
+                            Image.asset('assets/flower.webp', width: 50, height: 50),
+                            Text('꽃 구함'),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+                          ]
+                      ),
+                      Column(
+                          children: [
+                            Image.asset('assets/drum.jpg', width: 50, height: 50),
+                            Text('드럼 구함'),
+                          ]
+                      ),
+                    ],
+                  ),
+                  // 아래 메인 작성
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('동네모집', style: TextStyle(backgroundColor: Colors.grey, fontSize: 10),),
+                              Text('강남역 자취 10년차의 맛집 리스트'),
+                              Text('저만 알고 싶은 맛집 리스트 공유해요', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                              Text('역삼동 . 2분 전 . 조회 30', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                              ],
+                          ),
+                          Column(
+                            children: [
+                              Image.asset('assets/nuddle.png',width: 80,height: 80,),
+                              //아이콘 Row
+                              Row(
+                                children: [
+                                  Icon(Icons.thumb_up_alt_outlined),
+                                  Text('3'),
+                                  Icon(Icons.message_outlined),
+                                  Text('24'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('동네모집', style: TextStyle(backgroundColor: Colors.grey, fontSize: 10),),
+                              Text('강남역 자취 10년차의 맛집 리스트'),
+                              Text('저만 알고 싶은 맛집 리스트 공유해요', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                              Text('역삼동 . 2분 전 . 조회 30', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Image.asset('assets/nuddle.png',width: 80,height: 80,),
+                              //아이콘 Row
+                              Row(
+                                children: [
+                                  Icon(Icons.thumb_up_alt_outlined),
+                                  Text('3'),
+                                  Icon(Icons.message_outlined),
+                                  Text('24'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('동네모집', style: TextStyle(backgroundColor: Colors.grey, fontSize: 10),),
+                              Text('강남역 자취 10년차의 맛집 리스트'),
+                              Text('저만 알고 싶은 맛집 리스트 공유해요', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                              Text('역삼동 . 2분 전 . 조회 30', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Image.asset('assets/nuddle.png',width: 80,height: 80,),
+                              //아이콘 Row
+                              Row(
+                                children: [
+                                  Icon(Icons.thumb_up_alt_outlined),
+                                  Text('3'),
+                                  Icon(Icons.message_outlined),
+                                  Text('24'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('동네모집', style: TextStyle(backgroundColor: Colors.grey, fontSize: 10),),
+                              Text('강남역 자취 10년차의 맛집 리스트'),
+                              Text('저만 알고 싶은 맛집 리스트 공유해요', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                              Text('역삼동 . 2분 전 . 조회 30', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Image.asset('assets/nuddle.png',width: 80,height: 80,),
+                              //아이콘 Row
+                              Row(
+                                children: [
+                                  Icon(Icons.thumb_up_alt_outlined),
+                                  Text('3'),
+                                  Icon(Icons.message_outlined),
+                                  Text('24'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('동네모집', style: TextStyle(backgroundColor: Colors.grey, fontSize: 10),),
+                              Text('강남역 자취 10년차의 맛집 리스트'),
+                              Text('저만 알고 싶은 맛집 리스트 공유해요', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                              Text('역삼동 . 2분 전 . 조회 30', style: TextStyle(color: Colors.grey, fontSize: 10),),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Image.asset('assets/nuddle.png',width: 80,height: 80,),
+                              //아이콘 Row
+                              Row(
+                                children: [
+                                  Icon(Icons.thumb_up_alt_outlined),
+                                  Text('3'),
+                                  Icon(Icons.message_outlined),
+                                  Text('24'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+                    ]
+                  ),
+                ],
+              ),
         ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+           height: 150,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.home_filled, color: Colors.black, size: 50,),
+                Icon(Icons.sticky_note_2_sharp, color: Colors.black,size: 50,),
+                Icon(Icons.location_on_outlined, color: Colors.black,size: 50,),
+                Icon(Icons.chat_outlined, color: Colors.black,size: 50,),
+                Icon(Icons.account_circle_outlined, color: Colors.black,size: 50,)
+              ],
+            ),
+
+          ),
+        ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
   }
 }
